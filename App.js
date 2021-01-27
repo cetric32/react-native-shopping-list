29415f043cd3b95b78b3e90f231158686a454773
Cetric Lihalakha <cetric@hava.bz>
2021-01-27 09:49:58 +0300

    save to file

--- App.js
+++ App.js
@@ -1,17 +1,32 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {StyleSheet, FlatList, SafeAreaView} from 'react-native';
 import AddItem from './components/AddItem';
 import Header from './components/Header';
 import ListItem from './components/ListItem';
+import RNFS from 'react-native-fs';
 
 export default function App() {
-  const [items, setItems] = useState([
-    {id: Math.random().toString(), text: 'Milk'},
-    {id: Math.random().toString(), text: 'Eggs'},
-    {id: Math.random().toString(), text: 'Lemons'},
-    {id: Math.random().toString(), text: 'Cheese'},
-    {id: Math.random().toString(), text: 'Choma'},
-  ]);
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    let path = RNFS.DocumentDirectoryPath + '/shopping.json';
+    RNFS.exists(path)
+      .then((result) => {
+        if (result) {
+          // if we have a file, read it
+          return RNFS.readFile(path);
+        } else {
+          return false;
+        }
+      })
+      .then((content) => {
+        console.log(content);
+        if (content) {
+          setItems(JSON.parse(content));
+        }
+      })
+      .catch((err) => console.log(err.message, err.code));
+  }, []);
 
   const deleteItem = (id) => {
     setItems((prevItems) => {
@@ -19,10 +34,38 @@ export default function App() {
         return item.id !== id;
       });
     });
+
+    save_data_to_file();
+  };
+
+  const save_data_to_file = () => {
+    let path = RNFS.DocumentDirectoryPath + '/shopping.json';
+
+    RNFS.exists(path)
+      .then((result) => {
+        return result;
+      })
+      .then((content) => {
+        if (content) {
+          return RNFS.unlink(path);
+        } else {
+          return true;
+        }
+      })
+      .then(() => {
+        return RNFS.writeFile(path, JSON.stringify(items));
+      })
+      .then((success) => {
+        console.log('FILE WRITTEN!');
+      })
+      .catch((err) => {
+        console.log(err.message, err.code);
+      });
   };
 
   const addNewItem = (text) => {
     setItems([{id: Math.random().toString(), text: text}, ...items]);
+    save_data_to_file();
   };
 
   return (
